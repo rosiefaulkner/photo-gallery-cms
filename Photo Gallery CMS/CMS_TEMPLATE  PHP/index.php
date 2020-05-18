5c4c4c5c4440755319a3beafe7142f0e5f7958f2
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.local>
2020-05-18 00:31:46 -0400

    initial bootstrap files

---
+++ Photo Gallery CMS/CMS_TEMPLATE  PHP/index.php
@@ -0,0 +1,30 @@
+<?php include("includes/header.php"); ?>
+
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+    
+            
+          
+         
+
+            </div>
+
+
+
+
+            <!-- Blog Sidebar Widgets Column -->
+            <div class="col-md-4">
+
+            
+                 <?php include("includes/sidebar.php"); ?>
+
+
+
+        </div>
+        <!-- /.row -->
+
+        <?php include("includes/footer.php"); ?>
