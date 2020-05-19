c7fd36111c60e454da060c08e31879785d5e384b
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.local>
2020-05-18 20:12:18 -0400

    adding pages and admin

---
+++ Photo Gallery CMS/admin/includes/side_nav.php
@@ -0,0 +1,21 @@
+<!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li>
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="users.php"><i class="fa fa-fw fa-bar-chart-o"></i>Users</a>
+                    </li>
+                    <li>
+                        <a href="upload.php"><i class="fa fa-fw fa-table"></i>Upload</a>
+                    </li>
+                    <li>
+                        <a href="comments.php"><i class="fa fa-fw fa-edit"></i>Comments</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        
