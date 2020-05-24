d9a9ddac3d9596bcf99357b9c7773cdb54c52c95
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.attlocal.net>
2020-05-24 00:03:00 -0400

    adding login and logout methods

--- Photo Gallery CMS/public/admin/includes/init.php
+++ Photo Gallery CMS/public/admin/includes/init.php
@@ -1,6 +1,7 @@
 <?php
 
-include("new_config.php");
-include("database.php");
-//include("user.php");
-include("functions.php");
+require_once("new_config.php");
+require_once("database.php");
+require_once("user.php");
+require_once("functions.php");
+require_once("session.php");
